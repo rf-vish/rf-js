f5e92c7e635f54cc771f05fb973bfe799b9b40a2
rf-vish <128125924+rf-vish@users.noreply.github.com>
2024-09-26 15:02:48 +0530

    Update contactForm.js

--- contactForm.js
+++ contactForm.js
@@ -3,11 +3,11 @@ jQuery(function ($) {
   const parsedUrl = new URL(window.location.href);
   const params = parsedUrl.search.split('/');
   const userId = params[1]; // Extract the userId
-  const resourceID = params[2]; // Extract the resourceID
+  const resourceId = params[2]; // Extract the resourceID
   const contactId = params[3]; // Extract the contactId
 
   console.log('User ID: ' + userId);
-  console.log('Resource ID: ' + resourceID);
+  console.log('Resource ID: ' + resourceId);
   console.log('Contact ID: ' + contactId);
 
   // If contactId exists, make an API call to get contact details
@@ -31,9 +31,21 @@ jQuery(function ($) {
     console.log('No contactId found in the URL.');
   }
 
+  function todayDate() {
+    const today = new Date();
+    return today.toISOString(); // Adjust the format as needed
+  }
+
   // Handle form submission
   $('#contactForm').on('submit', function (event) {
     event.preventDefault(); // Prevent the default form submission behavior
+    $(':button').attr('disabled', true);
+
+    // Get additional attributes from the form
+    const contactForm = document.getElementById('contactForm');
+    const labelID = contactForm.getAttribute('labelid');
+    const campaignID = contactForm.getAttribute('campaignid');
+    const redirectUrl = contactForm.getAttribute('redirect');
 
     // Collect form data for submission
     const formData = {
@@ -42,19 +54,30 @@ jQuery(function ($) {
       email: $('#email').val(),
       phone: $('#phone').val(),
       note: $('#note').val(),
-      userId: userId, // Include userId from the URL
-      resourceID: resourceID, // Include resourceID from the URL
-      contactId: contactId, // Include contactId from the URL
+      resourceID: resourceId,
+      noteTimeStamps: [todayDate()],
+      contactTag: labelID,
+      campaign: campaignID || '',
+      resourceId: resourceId,
+      senderId: userId,
+      sentFrom: 'customPage',
     };
+
+    console.log(formData);
 
     // Submit the form data to the API
     $.ajax({
-      url: 'https://apiv2.rapidfunnel.com/v2/contact-details/' + contactId, // Replace with your submission API endpoint
+      url: 'https://my.rapidfunnel.com/landing/resource/create-custom-contact',
       method: 'POST',
       contentType: 'application/json',
       data: JSON.stringify(formData), // Convert form data to JSON
       success: function (response) {
-        alert('Form submitted successfully!');
+        console.log(response);
+        if (response.contactId > 0) {
+          alert('Form submitted successfully!');
+        } else {
+          alert('A contact could not be added!');
+        }
         // Optionally, redirect or reset the form
         // window.location.href = "https://yourdomain.com/thank-you";  // Redirect after success
       },
